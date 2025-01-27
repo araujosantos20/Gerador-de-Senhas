92d7228881edbc71198099664c24a945d4404d67
Mateus Santos <mateus.sanjo20@gmail.com>
2025-01-26 22:49:48 -0300

    Generador de senhas finalizado com a função de gerar senhas aleatórias e o botão de copiar a senha

--- src/components/BatButton/BatButton.tsx
+++ src/components/BatButton/BatButton.tsx
@@ -3,12 +3,19 @@ import { Button, View, Text, Pressable } from "react-native";
 
 import { styles } from "./BatButtonStyles";
 import { BatInput } from "../batinput/BatInput";
+import generatorPassword from "../../services/generatorPass";
+import * as Clipboard from "expo-clipboard";
 
 export function BatButton() {
   const [pass, setPass] = useState("");
 
   function handleGeneratePass() {
-    setPass("Olá");
+    let generateToken = generatorPassword();
+    setPass(generateToken);
+  }
+
+  function handleCopyPass() {
+    Clipboard.setStringAsync(pass);
   }
   return (
     <>
@@ -16,10 +23,7 @@ export function BatButton() {
       <Pressable style={styles.button} onPress={handleGeneratePass}>
         <Text style={styles.text}>GENERATE</Text>
       </Pressable>
-      <Pressable
-        style={styles.button}
-        onPress={() => console.log("Fui pressionado")}
-      >
+      <Pressable style={styles.button} onPress={handleCopyPass}>
         <Text style={styles.text}>☀ Copy</Text>
       </Pressable>
     </>
